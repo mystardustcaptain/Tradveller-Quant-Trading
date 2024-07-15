ae5cfaf5ef338a9563f97fc64f5efc9900fd79f7
Matt Tey <stand.by.matt@gmail.com>
2024-07-15 19:49:13 +0800

    enhance and simplify code for easier data extraction

--- candle_topic_data_extraction.py
+++ candle_topic_data_extraction.py
@@ -13,10 +13,15 @@ import logging
 import colorlog
 import pandas as pd
 
+TIMEFRAME = "1h"
+EXCHANGE_TOPIC = "bybit"
+CRYPTO_SYMBOL = "BTC"
+
 class Strategy(BaseStrategy):
-    datasource_data = []
+    # datasource_data = []
     candle_data = []
     start_time = datetime.utcnow()
+
     def __init__(self):
         handler = colorlog.StreamHandler()
         handler.setFormatter(
@@ -26,47 +31,38 @@ class Strategy(BaseStrategy):
         file_handler.setLevel(logging.INFO)
         super().__init__(log_level=logging.INFO, handlers=[handler, file_handler])
 
-    async def set_param(self, identifier, value):
-        logging.info(f"Setting {identifier} to {value}")
-        if identifier == "sma":
-            self.sma_length = int(value)
-        elif identifier == "z_score":
-            self.z_score_threshold = float(value)
-        else:
-            logging.error(f"Could not set {identifier}, not found")
-
     async def on_candle_closed(self, strategy, topic, symbol):
         # Retrieve list of candle data for corresponding symbol that candle closed.
         logging.info("candle data {}".format(super().data_map[topic][-1]))
         self.candle_data.append(super().data_map[topic][-1])
             
-    async def on_datasource_interval(self, strategy: StrategyTrader, topic: str, data_list):
-        logging.info("datasource data {}".format(super().data_map[topic][-1]))
-        self.datasource_data.append(super().data_map[topic][-1])
+    # async def on_datasource_interval(self, strategy: StrategyTrader, topic: str, data_list):
+    #     logging.info("datasource data {}".format(super().data_map[topic][-1]))
+    #     self.datasource_data.append(super().data_map[topic][-1])
 
     async def on_backtest_complete(self, strategy: StrategyTrader):
         # df = pd.DataFrame(self.datasource_data)
         # df.to_csv("coinglass_openinterest_ohlc_binance_BTC_4h.csv")
 
         df = pd.DataFrame(self.candle_data)
-        df.to_csv("bybit_candle_btc_1d.csv")
+        df.to_csv(f"{EXCHANGE_TOPIC}_candle_{CRYPTO_SYMBOL}_{TIMEFRAME}.csv")
 
         time_taken = datetime.utcnow() - self.start_time
         print("Total time taken: ", time_taken)
 
 config = RuntimeConfig(
-            mode=RuntimeMode.Backtest,
-            datasource_topics=[],
-            candle_topics=["candles-1d-BTC/USDT-bybit"],
-            active_order_interval=1,
-            initial_capital=10_000.0,
-            exchange_keys="./asdfasd.json",
-            start_time=datetime(2021, 1, 1, 0, 0, 0, tzinfo=timezone.utc),
-            end_time=datetime(2024, 4, 1, 5, 0, 0, tzinfo=timezone.utc),
-            data_count=100,
-            api_key="test",
-            api_secret="notest",
-        )
+    mode=RuntimeMode.Backtest,
+    datasource_topics=[],
+    candle_topics=[f"candles-{TIMEFRAME}-{CRYPTO_SYMBOL}/USDT-{EXCHANGE_TOPIC}"],
+    active_order_interval=1,
+    initial_capital=10_000.0,
+    exchange_keys="./asdfasd.json",
+    start_time=datetime(2021, 1, 1, 0, 0, 0, tzinfo=timezone.utc),
+    end_time=datetime(2024, 4, 1, 5, 0, 0, tzinfo=timezone.utc),
+    data_count=100,
+    api_key="test",
+    api_secret="notest",
+)
 
 permutation = Permutation(config)
 hyper_parameters = {}
